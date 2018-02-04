48aa1f1d0fafc46ba72e02134c0b18b6e91e3c03
Xander Evans <monkeywizardx@gmail.com>
2018-02-04 12:00:49 -0600

    Added some defaults for graceful failure.

--- typeclasses/spells.py
+++ typeclasses/spells.py
@@ -18,14 +18,20 @@ class Spell:
     }
     damage_types = [] # Keys corresponding to the damage types.
 
-    def __init__(self, key, skill_requirement, damage, skill_boost, damage_types, lore=""):
+    def __init__(self,
+        key,
+        skill_requirement = None,
+        damage = 10,
+        skill_boost = None,
+        damage_types = None,
+        lore=""):
         spells[key] = self
         self.key = key
-        self.skill_requirement = skill_requirement
+        self.skill_requirement = skill_requirement or self.skill_requirement
         self.damage = damage
-        self.skill_boost = skill_boost
-        self.damage_types = damage_types
-
+        self.skill_boost = skill_boost or self.skill_boost
+        self.damage_types = damage_types or ['PHYSICAL']
+        self.lore = lore
     def description(self):
         return '''
         Skill: {}
@@ -45,6 +51,8 @@ class Spell:
         caster.msg("You cast %s on %s." % self.key, target)
         boost = sum([level * self.skill_boost.get(skill, 0) for skill, level in caster.db.skills])
         target.db.damage(self.damage + boost, damage_types)
+        for skill in self.skill_boost:
+            caster.db.skills[skill] += 0.1
 
 import yaml
 for spell_name, spell in yaml.safe_load(open('skills/skills.yaml')).items:
